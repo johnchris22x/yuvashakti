a1fa4ba8fbc7b67e1f215bec2665e9047e46d29c
Mayank Diwaker <62379947+mayanksmind@users.noreply.github.com>
2021-02-07 20:45:33 +0530

    File

---
+++ yuva.sh
@@ -0,0 +1,13 @@
+apt update
+/*Provided by yuvashakti-sh.blogspot.com*/
+apt upgrade -y
+pkg install git -y
+pkg install fish -y
+pkg install python2 -y
+/*Provided by yuvashakti-sh.blogspot.com*/
+apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh
+git clone https://github.com/Zucccs/PhoneSploit
+cd PhoneSploit
+pip2 install colorama
+python2 main_linux.py -y
+/* ONLY FOR EDUCATIONAL PUROPST*/
